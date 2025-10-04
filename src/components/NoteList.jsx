65338ed9640d4bcae19975beb34c6675d8d9fad3
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-10-04 11:37:46 +0530

    Refactor NoteList to utilize Note component for rendering individual notes

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import Note from "./Note";
+
 const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
     return <p className='text-center text-gray-500 mt-10'>No Notes Yet</p>;
@@ -6,44 +8,7 @@ const NoteList = ({ notes, deleteNote }) => {
   return (
     <div className='space-y-4'>
       {notes.map((note) => {
-        return (
-          <div
-            key={note.id}
-            className='p-4 bg-[#2f2f2f] rounded-lg shadow-md border-l-4 border-blue-500 mt-10 relative'
-          >
-            <div className='flex justify-between items-start'>
-              <h3 className='text-lg font-bold text-gray-100 pr-4'>
-                {note.title}
-              </h3>
-              <div className='flex gap-2 flex-shrink-0'>
-                <span className='text-xs px-2 py-1 bg-[#3f3f3f] rounded-lg text-gray-300 border border-gray-600'>
-                  {note.priority === "High"
-                    ? "🔴"
-                    : note.priority === "Medium"
-                    ? "🟠"
-                    : "🟢"}{" "}
-                  {note.priority}
-                </span>
-                <span className='text-xs px-2 py-1 bg-[#3f3f3f] rounded-lg text-gray-300 border border-gray-600'>
-                  {note.category === "Work"
-                    ? "📁"
-                    : note.category === "Personal"
-                    ? "🏡"
-                    : "💡"}{" "}
-                  {note.category}
-                </span>
-              </div>
-            </div>
-            <p className='mt-3 text-gray-400'>{note.description}</p>
-
-            <button
-              onClick={() => deleteNote(note.id)}
-              className='mt-3 text-red-500 cursor-pointer transtion hover:text-red-700'
-            >
-              🗑️ Delete
-            </button>
-          </div>
-        );
+        return <Note key={note.id} note={note} deleteNote={deleteNote} />;
       })}
     </div>
   );
